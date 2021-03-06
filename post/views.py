d0aed0d3257ae31f90ee9c1a1d854ce2fe51942a
Hassan <mhas4908@gmail.com>
2021-03-06 09:10:32 +0500

    update detail_blog

--- post/views.py
+++ post/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-def home(request):
-	return render(request, 'home.html', {})
+class HomeView(ListView):
+	model = Post
+	template_name = 'home.html'
+
+class PostView(DetailView):
+	model = Post
+	template_name = 'post_detail.html'
