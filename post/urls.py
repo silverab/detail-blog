cbcb8fe58377a25f6ba9b362a4a2b1cc82df2edd
Hassan <mhas4908@gmail.com>
2021-03-07 09:36:27 +0500

    edit

--- post/urls.py
+++ post/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 # from . import views
-from .views import HomeView, PostView, AddPostView
+from .views import HomeView, PostView, AddPostView, UpdatePostView
 
 
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('Post/<int:pk>', PostView.as_view(), name='post_detail'),
     path('add_post/', AddPostView.as_view(), name='add_post'),
+    path('Post/edit/<int:pk>/', UpdatePostView.as_view(), name='update_post'),
 ]
--- post/views.py
+++ post/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, EditForm
 
 class HomeView(ListView):
 	model = Post
@@ -17,3 +17,9 @@ class AddPostView(CreateView):
 	template_name = 'post/add_post.html'
 	# fields = '__all__'
 	# fields = ('title', 'body')
+
+class UpdatePostView(UpdateView):
+	model = Post
+	form_class = EditForm
+	template_name = 'post/update_post.html'
+	# fields = ['title', 'title_tag', 'body']
