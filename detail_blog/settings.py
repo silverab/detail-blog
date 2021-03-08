24d2bedfb5c186f205e34aa7657b9da12a7f3bbb
Hassan <mhas4908@gmail.com>
2021-03-08 08:46:45 +0500

    add new app

--- detail_blog/settings.py
+++ detail_blog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'post',
+    'members',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'home'
+
+LOGOUT_REDIRECT_URL = 'home'
--- detail_blog/urls.py
+++ detail_blog/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('post.urls')),
+    path('members/', include('members.urls')),
+    path('members/', include('django.contrib.auth.urls')),
 ]
