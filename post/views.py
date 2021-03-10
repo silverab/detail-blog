3c75f193167323cf4f78ace998dc6b7af36e3ebc
Hassan <mhas4908@gmail.com>
2021-03-10 19:29:49 +0500

    updates

--- post/views.py
+++ post/views.py
@@ -13,7 +13,14 @@ class HomeView(ListView):
 	model = Post
 	template_name = 'post/home.html'
 	ordering = ['-post_date']
-	ordering = ['-id']
+	# ordering = ['-id']
+
+	def get_context_data(self, *args, **kwargs):
+		cat_menu = Category.objects.all()
+		context = super(HomeView, self).get_context_data(*args, **kwargs)
+		context["cat_menu"] = cat_menu
+		return context
+
 
 class PostView(DetailView):
 	model = Post
